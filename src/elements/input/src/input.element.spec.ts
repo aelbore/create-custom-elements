031157d01aea7d2fb3935eb91a26918ee35555e5
Jose Martin Llaneza <martinllaneza@gmail.com>
2018-10-27 13:37:21 +0800

    test: add test cases for input

--- src/elements/input/src/input.element.spec.ts
+++ src/elements/input/src/input.element.spec.ts
@@ -16,4 +16,60 @@ describe('ARElement', () => {
     expect(element).toBeTruthy();
   })
 
+  it('should change placeholder attribute', () => {
+    element.placeholder = 'First Name';
+    expect(element.labelElement.innerHTML).toEqual('First Name');
+    
+    element.setAttribute('placeholder', 'Last Name');
+    expect(element.labelElement.innerHTML).toEqual('Last Name');
+  })
+
+  it('should change disabled attribute', () => {
+    element.disabled = true;
+    expect(element.shadowRoot.querySelector('input').disabled).toBeTruthy();
+    
+    element.setAttribute('disabled', 'false');
+    expect(element.shadowRoot.querySelector('input').disabled).toBeFalsy();
+  })
+
+  it('should add/remove active class on label', () => {
+    element.placeholder = 'Name';
+    element.value = 'John Doe';
+    expect(element.labelElement.classList.contains('active')).toBeTruthy();
+
+    element.value = '';
+    expect(element.labelElement.classList.contains('active')).toBeFalsy();
+  })
+
+  it('should emit ar.change event', (done) => {
+    const inputEvent = new Event('keyup');
+    const inputElem = element.shadowRoot.querySelector('input');
+    element.value = 'keyup';
+    element.addEventListener('ar.change', e => {
+      expect(e.detail.value).toBeDefined('keyup');
+      done();
+    });
+    inputElem.dispatchEvent(inputEvent);
+  })
+
+  it('should emit ar.blur event', (done) => {
+    const inputEvent = new Event('blur');
+    const inputElem = element.shadowRoot.querySelector('input');
+    element.addEventListener('ar.blur', e => {
+      expect(e.detail).toBeDefined();
+      done();
+    });
+    inputElem.dispatchEvent(inputEvent);
+  })
+
+  it('should emit ar.focus event', (done) => {
+    const inputEvent = new Event('focus');
+    const inputElem = element.shadowRoot.querySelector('input');
+    element.addEventListener('ar.focus', e => {
+      expect(e.detail).toBeDefined();
+      done();
+    });
+    inputElem.dispatchEvent(inputEvent);
+  })
+
 })
