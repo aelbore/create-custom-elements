d93b9b27547614333215e083d9cf088d9d8a3460
aelbore <aelbore.sg@gmail.com>
2018-11-02 15:48:13 +0800

    use css grid for the grid, remove bootstrap dependencies

--- src/libs/profile-card/src/profile-card.element.ts
+++ src/libs/profile-card/src/profile-card.element.ts
@@ -1,4 +1,4 @@
-import { CustomElement, Prop, Watch } from 'custom-elements-ts';
+import { CustomElement, Prop, Watch, Listen } from 'custom-elements-ts';
 import { debounceTime, distinctUntilChanged, flatMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 
@@ -8,8 +8,7 @@ import { CardElement } from './card/card.element';
 @CustomElement({
   tag: 'profile-card',
   templateUrl: './profile-card.element.html',
-  styleUrl: './profile-card.element.scss',
-  shadow: false
+  styleUrl: './profile-card.element.scss'
 })
 export class ProfileCardElement extends HTMLElement {
 
@@ -18,9 +17,9 @@ export class ProfileCardElement extends HTMLElement {
   @Watch('profiles')
   async onPropertyChangedProfiles(value: any) {
     if (value.new && Array.isArray(value.new)) {
-      const container = this.querySelector('div.container');
+      const container = this.shadowRoot.querySelector('div.card-container-list');
 
-      const children = Array.from(this.querySelectorAll('ar-card'));
+      const children = Array.from(this.shadowRoot.querySelectorAll('ar-card'));
       await Promise.all(children.map(child => child.remove()));      
       
       const emptyState = container.querySelector('.empty-state');
@@ -34,7 +33,6 @@ export class ProfileCardElement extends HTMLElement {
         }
         Array.from(value.new).forEach((card: Profile) => {
           const element = <CardElement>document.createElement('ar-card');
-          element.classList.add(`col-md-3`)
           element.profile = card;
   
           container.appendChild(element);
@@ -42,28 +40,28 @@ export class ProfileCardElement extends HTMLElement {
       }
     }
   }
+  
+  @Listen('ar.change', 'ar-input')
+  onInputChanged(e: CustomEvent | any) {
+    of(e.detail.value)
+      .pipe(debounceTime(700), distinctUntilChanged())
+      .pipe(flatMap(text => getProfiles(text)))
+      .subscribe(profiles => {
+        this.profiles = profiles;
+      });  
+  }
 
   connectedCallback() {
-    const input: any = this.querySelector('ar-input');
+    const input: any = this.shadowRoot.querySelector('ar-input');
     getProfiles(input.value).subscribe(profiles => { 
       this.profiles = profiles;
     });
-
-    input.addEventListener('ar.change', function(e: CustomEvent | any) {
-      of(e.detail.value)
-        .pipe(debounceTime(700), distinctUntilChanged())
-        .pipe(flatMap(text => getProfiles(text)))
-        .subscribe(profiles => {
-          this.profiles = profiles;
-        });    
-    }.bind(this))
   }
 
   createEmptyState() {
     const template = document.createElement('template');
     template.innerHTML = `
-      <div class="empty-state"
-        style="text-align: center;border: 1px solid lightgray;padding: 20px">
+      <div class="empty-state">
         No Records Found.
       </div>
     `;
