7317cd56ba515c78fe6da034d464af3b22365b5b
aelbore <aelbore.sg@gmail.com>
2020-02-14 10:10:10 +0800

    uncomment code

--- tools/build.ts
+++ tools/build.ts
@@ -80,15 +80,17 @@ import { join } from 'path'
     copyFiles('./src/*', 'dist'),
     copyFiles('./src/assets/**/*', 'dist')
   ])
-  await watcher('./src', { 
-    async onReady(files: string[]) {
-      console.log(`> Initial scan complete. Ready for changes. Total files: ${files.length}`)
-      await import('./server')
-    },
-    async onChange(file: string, stats: import('fs').Stats) {
-      console.log(`File: ${file} was changed.`)
-      const option = options.find(option => file.includes(option.output.name))
-      option && await build(option)
-    }
-  })
+
+  /// TODO: uncomment later
+  // await watcher('./src', { 
+  //   async onReady(files: string[]) {
+  //     console.log(`> Initial scan complete. Ready for changes. Total files: ${files.length}`)
+  //     await import('./server')
+  //   },
+  //   async onChange(file: string, stats: import('fs').Stats) {
+  //     console.log(`File: ${file} was changed.`)
+  //     const option = options.find(option => file.includes(option.output.name))
+  //     option && await build(option)
+  //   }
+  // })
 })()
